eae810f82080730aab65ddfdd33314f92b03e086
Dallas Ng <41897965+Dallas-Ng@users.noreply.github.com>
2019-08-26 18:45:22 +0800

    Added key generator for encryptions

--- Steam Account Switcher/Models/Hash.cs
+++ Steam Account Switcher/Models/Hash.cs
@@ -10,6 +10,35 @@ namespace SteamAccountSwitcher.Models
 {
     class Hash
     {
+        internal static readonly char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+
+        private static string GetUniqueKey(int size)
+        {
+            byte[] data = new byte[4 * size];
+            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+            {
+                crypto.GetBytes(data);
+            }
+            StringBuilder result = new StringBuilder(size);
+            for (int i = 0; i < size; i++)
+            {
+                var rnd = BitConverter.ToUInt32(data, i * 4);
+                var idx = rnd % chars.Length;
+
+                result.Append(chars[idx]);
+            }
+
+            return result.ToString();
+        }
+
+        public static void CheckEncryptionKey()
+        {
+            if (Properties.Settings.Default.EncryptionKey == "Default")
+            {
+                Properties.Settings.Default.EncryptionKey = GetUniqueKey(10);
+            }
+        }
+
         private const string initVector = "tu89geji340t89u2";
 
         private const int keysize = 256;
